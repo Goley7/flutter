c192d84d1a0873d422f4f368ba3da48dc463a343
Goley <yadugole0@gmail.com>
2023-07-20 19:46:09 +0545

    This is me

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(IAmPoor());
+}
+
+class IAmPoor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Design type here is material app
+    return MaterialApp(
+      //home is a property
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // leading: Text('I am Poor'),
+        title: Text('I am Poor'),
+      ),
+      body: Text('Hello Boy'),
+      //appbar
+      //body
+      //bottom navigation bar
+    );
+  }
+}
